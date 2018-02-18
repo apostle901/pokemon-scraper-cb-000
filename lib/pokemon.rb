fb6f2dfb737b91db6386ba55ced2092013071355
Jawann <jawann@gmail.com>
2018-02-18 10:19:02 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,9 +3,9 @@ require 'pry'
 class Pokemon
   attr_accessor :id, :name, :type, :db, :hp
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db:, *hp)
     @id, @name, @type, @db = id, name, type, db
-    self.hp = 60
+    @hp = hp || 60
   end
 
   def self.save(name, type, db)
@@ -15,13 +15,12 @@ class Pokemon
   def self.find(id, db)
     hold = db.execute("SELECT * FROM pokemon WHERE (id = ?)", id)
     hold.flatten!
-    poke = Pokemon.new(id: hold[0], name: hold[1], type: hold[2], db: db)
+    poke = Pokemon.new(id: hold[0], name: hold[1], type: hold[2], db: db, hold[3])
     poke
   end
 
   def alter_hp(health, db)
     self.hp = health
     db.execute("UPDATE pokemon SET hp = (?) WHERE name = (?);", health, self.name)
-    binding.pry
   end
 end
