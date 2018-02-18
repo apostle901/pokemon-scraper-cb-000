9e7f5061f4794a37c314f839d5df89e4511c5bc3
Jawann <jawann@gmail.com>
2018-02-18 09:30:38 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,4 +15,11 @@ class Pokemon
     poke = Pokemon.new(id: hold[0], name: hold[1], type: hold[2], db: db)
     poke
   end
+
+  def alter_hp(health)
+    self.db.execute("UPDATE pokemon SET hp = health WHERE name = self.name;")
+  end
 end
+
+Once the hp column is set up there should be an instance method called alter_hp that will allow us to change a 
+specific pokemon's health to a new hp. It will need to take a new health power as a parameter.
